2e400be30982c2d182d2044f468c81358b485fef
Md Rijoan Maruf <rmsaif4430@gmail.com>
2025-07-08 03:48:47 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+// Load environment variables
+dotenv.config();
+
+// Create Express app
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+const user = process.env.DB_USER;
+const pass = process.env.DB_PASS;
+
+const uri = `mongodb+srv://${user}:${pass}@cluster0.0ykpaho.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+
+    const db = client.db('proFast');
+    const parcelCollection = db.collection('parcels')
+    // All API
+
+    // Post API - Create a new parcel
+    app.post('/parcels', async (req, res) => {
+      try {
+        const parcelData = req.body;
+        const result = await parcelCollection.insertOne(parcelData);
+        res.send(result);
+      } catch (error) {
+        res.status(500).send(error);
+      }
+    })
+
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("ProFast Server is running");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
